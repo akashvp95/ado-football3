52f371c1f024b092cfa6005bed399519617da167
akashvp95 <98869796+akashvp95@users.noreply.github.com>
2022-04-30 10:04:21 +0530

    Add files via upload

---
+++ tblFootBallController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FootBallProject3.Models;
+
+namespace FootBallProject3.Controllers
+{
+    public class tblFootBallController : Controller
+    {
+        ado_assignmentEntities db = new ado_assignmentEntities();
+        public ActionResult Index()
+        {
+            //List<tblFootball> result = db.tblFootballs.Where(x => x.Status == win).ToList();
+            //return View(result);
+            return View(db.tblFootballs.ToList());
+        }
+    }
+}
